6b01469104ba5c3233bfcfb166e52ed4d0b0adad
sushantjoshi800 <79572922+sjoshihypen@users.noreply.github.com>
2024-07-23 06:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config(page_title="Chat with Documents", page_icon=":book:")
-    st.header("Chat with PDF & Word Documents using Gemini🤷‍♂️")
+    st.header("Baat-E Docs using Gemini 🤷‍♂️")
     user_question = st.text_input("Ask a Question from the Documents 📑")
 
     if user_question:
